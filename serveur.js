1ea723431fca3081f402e980a7374fabf6392c7a
nico12450 <nicolas.gamel12450@gmail.com>
2019-08-04 14:40:38 +0200

    envoie des données du tableau vers le serveur

--- serveur.js
+++ serveur.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const fs = require('fs');
+const { parse } = require('querystring');
+const path = require('path');
+
+const FAVICON = path.join(__dirname, 'favicon.ico');
 
 const PORT=8080;
 
 var fichierHTML;
 
-fs.readFile('./JeuPlateau.html', function (err, html) {
+fs.readFile('./puissance4.html', function (err, html) {
 
     if (err) throw err;
 
@@ -20,13 +24,44 @@ http.createServer(function(request, response) {
 
 	if(adresse == "/favicon.ico"){
 
+		response.setHeader('Content-Type', 'image/x-icon');
+		fs.createReadStream(FAVICON).pipe(response);
+
 	}
 
 	else{
 
-		response.writeHeader(200, {"Content-Type": "text/html"});
-		response.write(fichierHTML);
-		response.end();
+		if(request.method == 'POST'){
+
+			var body = '';
+
+		    request.on('data', chunk => {
+		        body += chunk.toString();
+		    });
+
+		    request.on('end', () => {
+
+		    	var donnees = parse(body);
+		    	//console.log(donnees);
+
+		        var r = {
+	    			status  : 200,
+	    			success : 'Updated Successfully'
+				};
+
+		        response.end(JSON.stringify(r));
+		        
+		    });
+
+		}
+
+		else{
+
+			response.writeHeader(200, {"Content-Type": "text/html"});
+			response.write(fichierHTML);
+			response.end();
+
+		}
 
 	}
 
